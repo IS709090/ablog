8ab0fe721c958d8cdd8cc18008e6ac67c3707fbf
Diego Arana <is709090@iteso.mx>
2021-04-01 11:25:11 -0600

    models added

--- theblog/admin.py
+++ theblog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Event, MicroSitios
 # Register your models here.Category
 
+
 admin.site.register(Post)
 admin.site.register(Event)
 # admin.site.register(Category)
