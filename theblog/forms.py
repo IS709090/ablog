156c45ca77f0c02450461ded89d590e672945159
Diego Arana <is709090@iteso.mx>
2021-04-02 00:35:24 -0600

    cleaned dirs

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Post, Event, MicroSitios, Category
 
-# choices = Category.objects.all().values_list('name', 'name')
-# choice_list = []
+choices = Category.objects.all().values_list('name', 'name')
+choice_list = []
 
-# for item in choices:
-#     choice_list.append(item) 
+for item in choices:
+    choice_list.append(item) 
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -15,8 +15,8 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la publicación'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la publicación que veremos en la pestaña'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'type': 'hidden', 'id': 'user'}),            
-            #'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor de la publicación'}),
-            # 'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Categorías de la publicación'}),
+            'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor de la publicación'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Categorías de la publicación'}),
             'header_image': forms.ClearableFileInput(attrs={'class': 'form-control', 'required' : '' , 'placeholder': 'Imágen'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción o resumen de la publicación, que saldrá debajo de la publicación en el listado de publicaciones'}),
             #'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contenido de la publicación'}),
@@ -30,8 +30,8 @@ class EventForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del evento'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del evento que veremos en la pestaña'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'type': 'hidden', 'id': 'user'}),
-            #'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor del evento'}),
-            # 'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Categorías del evento'}),
+            'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor del evento'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Categorías del evento'}),
             'header_image': forms.ClearableFileInput(attrs={'class': 'form-control', 'required' : '' , 'placeholder': 'Imágen'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción o resumen del evento, que saldrá debajo del evento en el listado de eventos'}),
             #'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contenido del evento'}),
