43775642dd158e957925e9fc8a64ba6f3ff2d0d6
Diego Arana <is709090@iteso.mx>
2021-04-07 22:56:32 -0500

    forms updated

--- theblog/forms.py
+++ theblog/forms.py
@@ -10,49 +10,51 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'snippet', 'header_image', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'snippet', 'header_image', 'fileDownload', 'linkToFile', 'past_Publication_Date', 'category', 'body')
         category = forms.ChoiceField(choices=choice_list)
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la publicación'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la publicación que veremos en la pestaña'}),
             'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor de la publicación'}),
-            # 'category': forms.MultipleChoiceField(choices=choice_list),
-            # 'category': forms.ChoiceField(choices=choice_list, widget=forms.Select(attrs={'class':'bootstrap-select'})),
             'header_image': forms.ClearableFileInput(attrs={'class': 'form-control', 'required' : '' , 'placeholder': 'Imágen'}),
+            'fileDownload': forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Archivo a descargar'}),
+            'linkToFile': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Enlace a archivo descargable, si está almacenado en otra plataforma'}),
+            'past_Publication_Date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Fecha antigua de publicación, solo usar cuando se este migrando todo, ignorar si es publicación nueva. Formato Ejemplo: 7 de Abril de 2021'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción o resumen de la publicación, que saldrá debajo de la publicación en el listado de publicaciones'}),
-            #'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contenido de la publicación'}), attrs={'class': 'form-control', 'placeholder': 'Categorías de la publicación'}
         }
 
 
 class BlogTransversalPostForm(forms.ModelForm):
     class Meta:
         model = BlogTransversalPost
-        fields = ('title', 'title_tag', 'author', 'snippet', 'header_image', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'snippet', 'header_image', 'fileDownload', 'linkToFile', 'past_Publication_Date', 'category', 'body')
         category = forms.ChoiceField(choices=choice_list)
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del Blog'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del Blog que veremos en la pestaña'}),
             'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor del Blog'}),
-            # 'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Categorías del Blog'}),
             'header_image': forms.ClearableFileInput(attrs={'class': 'form-control', 'required' : '' , 'placeholder': 'Imágen'}),
+            'fileDownload': forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Archivo a descargar'}),
+            'linkToFile': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Enlace a archivo descargable, si está almacenado en otra plataforma'}),
+            'past_Publication_Date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Fecha antigua de publicación, solo usar cuando se este migrando todo, ignorar si es publicación nueva. Formato Ejemplo: 7 de Abril de 2021'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción o resumen del Blog, que saldrá debajo del Blog en el listado de Blog'}),
-            #'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contenido de la publicación'}),
         }
 
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ('title', 'title_tag', 'author', 'snippet', 'header_image', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'snippet', 'header_image', 'fileDownload', 'linkToFile', 'past_Publication_Date', 'category', 'body')
         category = forms.ChoiceField(choices=choice_list)
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del evento'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del evento que veremos en la pestaña'}),
             'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Autor del evento'}),
-            # 'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Categorías del evento'}),
             'header_image': forms.ClearableFileInput(attrs={'class': 'form-control', 'required' : '' , 'placeholder': 'Imágen'}),
+            'fileDownload': forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Archivo a descargar'}),
+            'linkToFile': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Enlace a archivo descargable, si está almacenado en otra plataforma'}),
+            'past_Publication_Date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'OPCIONAL. Fecha antigua de publicación, solo usar cuando se este migrando todo, ignorar si es publicación nueva. Formato Ejemplo: 7 de Abril de 2021'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción o resumen del evento, que saldrá debajo del evento en el listado de eventos'}),
-            #'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contenido del evento'}),
         }
 
 
